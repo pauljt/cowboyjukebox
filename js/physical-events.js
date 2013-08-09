32bb161205528a211ca1dde064b417f8e23005d5
Paul Theriault <ptheriault@mozilla.com>
2013-08-09 17:52:35 +1000

    Distance to point.

--- js/physical-events.js
+++ js/physical-events.js
@@ -1,21 +1,26 @@
 
-function distance(a, b) {
+function distance(a, lat, lon) {
   var R = 6371; // km
 
-  return Math.acos(Math.sin(a.coords.latitude)*Math.sin(b.coords.latitude) +
-                   Math.cos(a.coords.latitude)*Math.cos(b.coords.latitude) *
-                   Math.cos(b.coords.longitude-a.coords.longitude)) * R;
+  return Math.acos(Math.sin(a.latitude)*Math.sin(lat) +
+                   Math.cos(a.latitude)*Math.cos(lat) *
+                   Math.cos(lon-a.longitude)) * R;
 }
+
+// -28.228853527113206, 153.2699418067932
 
 var id, target, option;
 
 function success(pos) {
   var crd = pos.coords;
-  document.getElementById("lat").innerHTML = crd.latitude
-  document.getElementById("long").innerHTML = crd.longitude
+  document.getElementById("lat").innerHTML = crd.latitude;
+  document.getElementById("long").innerHTML = crd.longitude;
+  document.getElementById("dist").innerHTML = distance(crd, -28.228853527113206, 153.2699418067932);
 };
 
-setInterval(function(e){
-  navigator.geolocation.getCurrentPosition(success)
-},1000);
+navigator.geolocation.getCurrentPosition(success);
+
+// setInterval(function(e){
+navigator.geolocation.watchPosition(success);
+// },1000);
 
