ba4b9df521c06ad63099061a4b0e06242f5e6c4c
Paul Theriault <ptheriault@mozilla.com>
2013-08-09 16:49:03 +1000

    added geolocation

--- js/physical-events.js
+++ js/physical-events.js
@@ -31,3 +31,37 @@ window.addEventListener("deviceorientation", function (e) {
 window.addEventListener("compassneedscalibration", function (e) {
   document.getElementById("compassneedscalibration").innerHTML = "true";
 }, false);
+
+
+var id, target, option;
+
+function success(pos) {
+  var crd = pos.coords;
+
+  document.getElementById("lat").innerHTML = crd.latitude
+  document.getElementById("long").innerHTML = crd.longitude
+
+  if (target.latitude === crd.latitude && target.longitude === crd.longitude) {
+    console.log('Congratulation, you reach the target');
+    navigator.geolocation.clearWatch(id);
+  }
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+  document.getElementById("lat").innerHTML='ERROR(' + err.code + '): ' + err.message;
+  document.getElementById("long").innerHTML='ERROR(' + err.code + '): ' + err.message;
+};
+
+target = {
+  latitude : 0,
+  longitude: 0,
+}
+
+options = {
+  enableHighAccuracy: false,
+  timeout: 5000,
+  maximumAge: 0
+};
+
+id = navigator.geolocation.watchPosition(success, error, options);
